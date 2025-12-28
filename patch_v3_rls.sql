135128133cdb418f135ea3c1a8e06000315ac6e6
JreyForun <142200366+JreyForFun@users.noreply.github.com>
2025-12-28 22:43:39 +0800

    Delete patch_v3_rls.sql

--- patch_v3_rls.sql
+++
@@ -1,7 +0,0 @@
--- patch_v3_fix_rls.sql
-
--- The 'upsert' operation requires an UPDATE policy if the row exists.
--- Since we are anonymous, we allow updating any row (logic validation ensures we only update our own via session_id)
--- A more secure way would be to check a secret cookie, but for this UUID-based system, knowing the UUID is proof of ownership.
-
-CREATE POLICY "Public update own queue" ON match_queue FOR UPDATE USING (true);
